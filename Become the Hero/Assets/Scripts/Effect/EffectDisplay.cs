6e24081133a3a2b764f3f7be7e9a0be3547f9464
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2020-02-22 17:44:21 -0500

    Update 2/22
    Started scripting behavior for the Effect UI.

--- Become the Hero/Assets/Scripts/Effect/EffectDisplay.cs
+++ Become the Hero/Assets/Scripts/Effect/EffectDisplay.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewEffectDisplay", menuName = "Effect/UI/Effect Display", order = 3)]
 public class EffectDisplay : ScriptableObject
 {
+    public string displayName;
     public bool displayTurnLimit;
     public string description;
     public Sprite icon;
--- Become the Hero/Assets/Scripts/UI/UIEffectDisplay.cs
+++ Become the Hero/Assets/Scripts/UI/UIEffectDisplay.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIEffectDisplay : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI effectName;
+    [SerializeField]
+    private TextMeshProUGUI effectDescription;
+    [SerializeField]
+    private TextMeshProUGUI effectLimit;
+    [SerializeField]
+    private Image effectIcon;
+
     /// <summary>
     /// Initializes the display
     /// </summary>
-    public void Init()
+    public void Init(EffectInstance eff)
     {
         
     }
